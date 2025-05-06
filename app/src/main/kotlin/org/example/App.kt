816b9076092edf8d7820a6949a250778e4f937b3
ValenDilello <v.dilello@alumno.etec.um.edu.ar>
2025-05-06 08:50:17 -0300

    feat: Cálculo de raíces de la función

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,5 +1,5 @@
-
 package org.example
+import kotlin.math.sqrt
 
 fun main() {
     val a = 5
@@ -25,8 +25,34 @@ fun main() {
         val fx = calcularFuncion(a, b, c, x)
         println("$x\t$fx")
     }
+
+    println("\nCalculo de las raíces:")
+    calcularRaices(a.toDouble(), b.toDouble(), c.toDouble())
 }
 
 fun calcularFuncion(a: Int, b: Int, c: Int, x: Int): Int {
     return a * x * x + b * x + c
+}
+
+fun calcularRaices(a: Double, b: Double, c: Double) {
+    val discriminante = b * b - 4 * a * c
+    println("Discriminante (D) = %.2f".format(discriminante))
+
+    when {
+        discriminante > 0 -> {
+            val x1 = (-b + sqrt(discriminante)) / (2 * a)
+            val x2 = (-b - sqrt(discriminante)) / (2 * a)
+            println("Dos raíces reales diferentes:")
+            println("x₁ = %.2f".format(x1))
+            println("x₂ = %.2f".format(x2))
+        }
+        discriminante == 0.0 -> {
+            val x = -b / (2 * a)
+            println("Una raíz real doble:")
+            println("x = %.2f".format(x))
+        }
+        else -> {
+            println("No tiene raíces reales.")
+        }
+    }
 }
